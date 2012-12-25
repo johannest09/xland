50da13f08de7d26758e6c98bc466f9f0e2388cce
johannes <johannesfreyr@gmail.com>
2012-12-25 12:02:55 +0000

    adding jason files and js file modified

---
+++ javascript/places.js
@@ -0,0 +1,38 @@
+var stofur = [
+	{
+		"name" : "Landslag",
+		"address" : "Skólavörðustíg 11",
+		"postal" : 101,
+		"tel" : "535 5300",
+		"Fax" : "535 5301",
+		"markers": [{"lat": 64.145279, "lng": -21.930992}],
+		"url" : "www.landslag.is",
+		"description" : "Landslag er flott stofa",
+		"logo" : ""
+	},
+	{
+		"name" : "Forma ehf",
+		"address" : "Borgartún 6",
+		"postal" : 105,
+		"tel" : "587 5020",
+		"fax" : "587 5020",
+		"email" : "ingarut@forma.is",
+		"markers": [{"lat": 64.14578, "lng": -21.903054}],
+		"url" : "www.forma.is",
+		"description" : "Forma er flott stofa",
+		"logo" : ""
+	},
+	{
+		"name" : "Hornsteinar arkitektar ehf",
+		"address" : "Ingólfsstræti 5",
+		"postal" : 101,
+		"tel" : "511 7011",
+		"fax" : "511 7001",
+		"email" : "hornsteinar@hornsteinar.is",
+		"markers": [{"lat": 64.147829, "lng": -21.932923}],
+		"url" : "www.hornsteinar.is",
+		"description" : "Forma er flott stofa",
+		"logo" : ""
+	}
+	
+];
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,14 +8,77 @@
 
 var xland = {
 	
+	
+	
 	init: function() 
 	{
+		/* styles */
+		var reykjavik = new google.maps.LatLng(64.138621,-21.894722);
+
+		var MY_MAPTYPE_ID = 'reykjavikMap';
+		
+		var styles = [
+		  {
+		    "featureType": "landscape",
+		    "stylers": [
+		      { "color": "#d5d6d6" },
+		      { "weight": 0.1 }
+		    ]
+		  },{
+		    "featureType": "road.highway",
+		    "stylers": [
+		      { "color": "#5b5b5b" }
+		    ]
+		  },{
+		    "featureType": "road.arterial",
+		    "stylers": [
+		      { "color": "#bfc1c2" }
+		    ]
+		  },{
+		    "featureType": "road.local",
+		    "stylers": [
+		      { "color": "#ffffff" }
+		    ]
+		  },{
+		    "featureType": "water",
+		    "stylers": [
+		      { "color": "#a1a1a1" }
+		    ]
+		  },{
+		    "featureType": "poi.park",
+		    "stylers": [
+		      { "saturation": -100 }
+		    ]
+		  },{
+		    "featureType": "poi",
+		    "stylers": [
+		      { "hue": "#77ff00" },
+		      { "lightness": -12 },
+		      { "saturation": -58 }
+		    ]
+		  }
+		];
+		
+		
 		var myOptions = {
-		  center: new google.maps.LatLng(64.138621,-21.894722),
-		  zoom: 13,
-		  mapTypeId: google.maps.MapTypeId.ROADMAP
+			center: new google.maps.LatLng(64.138621,-21.894722),
+		 	zoom: 13,
+		 	/* mapTypeId: google.maps.MapTypeId.ROADMAP, */
+			mapTypeControlOptions: {
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+			},
+			mapTypeId: MY_MAPTYPE_ID
+			
 		};
 		var map = new google.maps.Map(document.getElementById("map-container"), myOptions);
+		
+		var styledMapOptions = {
+			name: 'X-land'
+        };
+
+		var jayzMapType = new google.maps.StyledMapType(styles, styledMapOptions);
+		
+		map.mapTypes.set(MY_MAPTYPE_ID, jayzMapType);
 	}
 };
 
